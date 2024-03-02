d16fb21422fa8fc68e4ea32787d2a6c984242a23
arshbhushan <arshbhushan@gmail.com>
2024-03-02 14:47:46 +0530

    AddedOurProduct_Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import HorizontalScroller from './components/HorizontalScroller';
 import About from './components/About';
 import OurGoals from './components/OurGoals';
-
+import OurProduct from './components/OurProduct.js';
 function App() {
   return (
     <>
@@ -12,6 +12,7 @@ function App() {
     <HorizontalScroller/>
     <About/>
     <OurGoals/>
+    <OurProduct/>
     </div>
     <div>hello</div>
     </>
